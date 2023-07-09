92408fcdc6a0ee4f23f00cae4c5341c4931194ed
Manol Naydenov <manol9705@gmail.com>
2023-07-09 20:16:41 +0300

    Replace find role by id with find role by role name

--- src/main/java/com/softuni/Pathfinder/repository/RoleRepository.java
+++ src/main/java/com/softuni/Pathfinder/repository/RoleRepository.java
@@ -2,11 +2,12 @@ package com.softuni.Pathfinder.repository;
 
 
 import com.softuni.Pathfinder.model.entity.RoleEntity;
+import com.softuni.Pathfinder.model.entity.enums.RoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
 
-    Optional<RoleEntity> findById(Long id);
+    Optional<RoleEntity> findByRole(RoleEnum role);
 }
